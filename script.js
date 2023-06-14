a50435e46cfc2958ce7b710d5536e4e344ac7175
Edit Selmeczi <selmeczi.e@gmail.com>
2023-06-14 10:39:41 +0200

    map countries and add them to HTML

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-console.log("loaded");
+const countryComponent = (country) =>
+  `<div class="country">
+  <h2>${country.name}</h2>
+  <h4>population: ${country.pop}</h4>
+  <h5>area: ${country.area}</h5>
+  <h3>languages: ${country.lang.join(" ")}</h3>
+  </div>`;
 
-fetch("https://restcountries.com/v3.1/all").then((res) =>
-  res.json().then((data) => console.log(data))
-);
+
+const init = async () => {
+  const rootElement = document.querySelector("#root");
+
+  const countriesData = await fetch("https://restcountries.com/v3.1/all").then(
+    (res) => res.json()
+  );
+const countries = countriesData.map(countryData => {
+  return{
+    name: countryData.name.common,
+    pop: countryData.population,
+    area: countryData.area,
+    lang: countryData.languages ? Object.keys(countryData.languages).map(lang => countryData.languages[lang]) : []
+
+  }
+});
+
+  //console.log(countries);
+ // countriesData.map((country) => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)));
+  countries.map((country) => rootElement.insertAdjacentHTML("beforeend", countryComponent(country)));
+};
+
+init();
